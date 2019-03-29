d46b4d3dbd311a6ee1bd657bde6ca49b0db6b02e
UkrAndy <brila_andrij@ukr.net>
2019-03-29 13:05:29 +0200

    Зауваження у тексті

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
-// Перше завдання
+// Перше завдання                         <-- Будь-ласка вставляйте умову задач в коментарях перед розв"язком
 function month(monthNumber) {
     let arr = ['Січень', 'Лютий', 'Березень', 'Квітень', 'Травень', 'Червень', 'Липень', 'Серпень', 'Вересень', 'Жовтень', 'Листопад', 'Грудень']
-    let str = arr[monthNumber - 1]
+    let str = arr[monthNumber - 1] тут не потрібно було описувати додаткову змінну. Одразу можна було поверати return arr[monthNumber - 1]
     return str
 }
 document.write(month(8))
 //Третє завдання
-function sum() {
+function sum() {        Ок
     let sum = 0
     for (let i = 0; i < arguments.length; i++) {
         sum += arguments[i]
@@ -31,14 +31,14 @@ function avg() {
 }
 document.write(`<br />${avg(7, 2, 3, 4, 5)}`)
 function min() {
-    return Math.min(...arguments)
+    return Math.min(...arguments)    <-- Будьте обережні з деструктуризацією (об"єкт може мати і інші властивості)
 }
 document.write(`<br />${min(7, 3, 4, 5)}`)
 //Четверте завдання
 function countEven(arr) {
     let count = 0
-    arr.forEach((x, i, arr) => {
-        (x % 2 == 0) ? count++ : false
+    arr.forEach((x, i, arr) => {                    <-- якщо параметри " i, arr" не використовуються, то можна їх взагалі не писати
+        (x % 2 == 0) ? count++ : false              <--- у даному випадку краще було використати reduce
     })
     return count
 }
@@ -46,17 +46,17 @@ let arr = [7, 3, 4, 5, 8, 66, 55]
 document.write(`<br />${countEven(arr)}`)
 // П'яте завдання
 function replaceZero(arr) {
-    let str = arr.join()
-    str = str.replace(/0/g, '1')
+    let str = arr.join()            <-- join праціює тільки з масивами рядкового типу
+    str = str.replace(/0/g, '1')    <-- виконайте спрочатку без регулярних виразів
     let one = str.split(',')
     return one
 }
 let arr2 = [10, 20, 30, 40]
 document.write(`<br />${replaceZero(arr2)}`)
 // Шосте завдання
-function table(a, b) {
+function table(a, b) {                            <-- Параметри називайте зрозумілиними назвами !!! (наприклад rowCount,colCount)
     let tbl = `<table border>`
-    for (let i = 0; i < a; i++) {
+    for (let i = 0; i < a; i++) {             
         tbl += `<tr>`
         for (let j = 0; j < b; j++) {
             tbl += `<td>${i + 1} ${j + 1}</td>`
